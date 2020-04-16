fd51f247662828999568fd182fc58543f0313ee2
rakib06 <six.rakib@gmail.com>
2020-04-16 08:42:20 +0600

    model admin

---
+++ gameplay/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Game, Move
+# Register your models here.
+
+#admin.site.register(Game)
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_player', 'second_player', 'status')
+
+
+admin.site.register(Move)
