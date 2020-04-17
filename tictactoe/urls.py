bb04d7f0590c437e77ee050ddd262ece62a092b5
rakib06 <six.rakib@gmail.com>
2020-04-17 15:09:52 +0600

    imported static files from initializr.com polished middle way

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^rockiblx/', admin.site.urls),
     url(r'^player/', include('player.urls')),
 ]
